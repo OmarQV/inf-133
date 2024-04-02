583a27e07d44eb904c595d43473dd30ba6cffd41
Omar <quispevargasomar@gmail.com>
2024-04-01 23:22:52 -0400

    sqlite3-try-catch

--- semana7/bd_sqlite/my_restaurant.py
+++ semana7/bd_sqlite/my_restaurant.py
@@ -6,15 +6,20 @@ conn = sqlite3.connect("restaurante.db")
 
 # ------------- PLATOS --------------
 # Crear tabla de platos
-conn.execute(
-   """
-   CREATE TABLE PLATOS
-   (id INTEGER PRIMARY KEY,
-   nombre TEXT NOT NULL,
-   precio REAL NOT NULL,
-   categoria TEXT NOT NULL);
-   """
-)
+try:
+   conn.execute(
+      """
+      CREATE TABLE PLATOS
+      (id INTEGER PRIMARY KEY,
+      nombre TEXT NOT NULL,
+      precio REAL NOT NULL,
+      categoria TEXT NOT NULL);
+      """
+   )
+except:
+   print("Error al crear tabla")
+
+
 # Insertar datos de platos
 # Plato 1
 conn.execute(
@@ -54,13 +59,17 @@ for row in cursor:
    
 # ------------- MESAS --------------
 # Crear tabla de mesas
-conn.execute(
-   """
-   CREATE TABLE MESAS
-   (id INTEGER PRIMARY KEY,
-   numero INTEGER NOT NULL);
-   """
-)
+try:
+   conn.execute(
+      """
+      CREATE TABLE MESAS
+      (id INTEGER PRIMARY KEY,
+      numero INTEGER NOT NULL);
+      """
+   )
+except:
+   print("Error al crear tabla")
+
 # Insertar datos de platos
 # Mesa 1
 conn.execute(
@@ -100,18 +109,23 @@ for row in cursor:
 
 # ------------- PEDIDOS --------------
 # Crear tabla de pedidos
-conn.execute(
-   """
-   CREATE TABLE PEDIDOS
-   (id INTEGER PRIMARY KEY,
-   plato_id INTEGER NOT NULL,
-   mesa_id INTEGER NOT NULL,
-   cantidad INTEGER NOT NULL,
-   fecha DATE NOT NULL,
-   FOREIGN KEY (plato_id) REFERENCES PLATOS(id),
-   FOREIGN KEY (mesa_id) REFERENCES MESAS(id));
-   """
-)
+try:
+   conn.execute(
+      """
+      CREATE TABLE PEDIDOS
+      (id INTEGER PRIMARY KEY,
+      plato_id INTEGER NOT NULL,
+      mesa_id INTEGER NOT NULL,
+      cantidad INTEGER NOT NULL,
+      fecha DATE NOT NULL,
+      FOREIGN KEY (plato_id) REFERENCES PLATOS(id),
+      FOREIGN KEY (mesa_id) REFERENCES MESAS(id));
+      """
+   )
+except:
+   print("Error al crear tabla")
+
+
 # Pedido 1
 conn.execute(
    """
